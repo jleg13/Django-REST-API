f082f326aa329b9e73d67c65c860e09508a4be25
Joshua Le Gresley <joshualegresley@gmail.com>
2021-12-23 22:36:17 +1100

    update flake8

--- server/core/models.py
+++ server/core/models.py
@@ -16,7 +16,7 @@ class UserManager(BaseUserManager):
         user.set_password(password)
         user.save(using=self._db)
         return user
-    
+
     def create_superuser(self, email, password):
         """Create and save a new superuser"""
         user = self.create_user(email, password)
@@ -35,4 +35,3 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
     objects = UserManager()
     USERNAME_FIELD = 'email'
-    
--- server/core/tests/test_models.py
+++ server/core/tests/test_models.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 
 
 class ModelTests(TestCase):
-    
+
     def test_create_user_with_email_succesful(self):
         """"Test creating a new user with an email is successful"""
         email = 'test@test.com'
@@ -14,18 +14,18 @@ class ModelTests(TestCase):
         )
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
-        
+
     def test_new_user_email_normalized(self):
         """Test the email for a new user is normalized"""
         email = 'test@TEST.COM'
         user = get_user_model().objects.create_user(email, 'password123')
         self.assertEqual(user.email, email.lower())
-        
+
     def test_new_user_invalid_email(self):
         """Test creating user with no email raises error"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, 'password123')
-            
+  
     def test_create_new_superuser(self):
         """Test creating a new superuser"""
         user = get_user_model().objects.create_superuser(
@@ -33,4 +33,3 @@ class ModelTests(TestCase):
         )
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
-        
